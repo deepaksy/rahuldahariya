f39c2cb240c14065d7d394f22f491bb4f86e20dc
deepaksy <deepaksy28@gmail.com>
2021-07-13 18:09:20 +0530

    Change

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 let themeColor="#212121";
 let n=1;
 let color='';
+let theme = '<img src="./images/sunrise.svg">';
+
 function themeToggle() {
     if(n==1){
         themeColor='white';
         color='black';
+        theme='<img src="./images/sunset.svg">';
         n=0;
     }
     else{
         themeColor='black';
         color='white';
+        theme='<img src="./images/sunrise.svg">';
         n++;
     }
     //document.body.style.backgroundColor=themeColor;
     document.getElementById('main').style.backgroundColor=themeColor;
     document.getElementById('main').style.color=color;
-
+    document.getElementById('theme').innerHTML=theme;
 }
